4cf9f73b861c3bb472d1939b1258170d9c3b99b7
camlutz <camlutz@gmail.com>
2018-08-25 04:53:59 -0400

    push nightly changes

---
+++ resources/views/league/overview.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+            <div class="panel panel-default">
+                <div class="panel-heading">Leagues</div>
+
+                <div class="panel-body">
+                    League Placeholder List:
+                      <ul>
+                        <li>League 1</li>
+                        <li>League 2</li>
+                        <li>League 3</li>
+                      </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
