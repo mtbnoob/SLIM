b4955f4101da3b94f3eff4f90797dbfcbd2c047f
camlutz <camlutz@gmail.com>
2018-08-27 01:37:39 -0400

    Nightly commit: started league stuff

---
+++ database/migrations/2018_08_27_053505_create_leagues_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLeaguesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('leagues', function (Blueprint $table) {
+            //
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('leagues', function (Blueprint $table) {
+            //
+        });
+    }
+}
---
+++ resources/views/league/create.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+            <div class="panel panel-default">
+                <div class="panel-heading">Create a New League</div>
+                <div class="panel-body">
+                    
+                  <form role="form" method="POST" action="{{ url('/league/create') }}">
+                    {!! csrf_field() !!}
+                    <label>League Name:</label><br>
+                      <input type="text" value="League Name" name="league_name"><br>
+                    <label>Sport</label><br>
+                      <select name="league_sport">
+                        <option value="hockey">Hockey</option>
+                        <option value="football">Football</option>
+                        <option value="soccer">Soccer</option>
+                        <option value="basketball">Basketball</option>
+                        <option value="basball">Baseball</option>
+                      </select><br><br>
+                    
+                    <input type="submit" value="Create Team">
+                  </form>
+                  
+                  
+                  
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
